bca34bc0a62aeaf63d44e908e34e0908668a0b6e
Javier <jgonzal99@gmail.com>
2023-06-22 08:41:22 +0000

    22-06_commit1

---
+++ PolymorphismExample/src/com/MethodOverloadingExample.java
@@ -0,0 +1,11 @@
+package com;
+
+public class MethodOverloadingExample {
+
+	public static void main(String[] args) {
+		Operation o = new Operation();
+		o.add("10","20");
+
+	}
+
+}
---
+++ PolymorphismExample/src/com/Operation.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Operation {
+	void add() {
+		System.out.println(1+100);
+	}
+	void add(int x) {
+		System.out.println(x+100);		
+	}
+	void add(String x, String y) {
+		System.out.println(x+y);		
+	}
+}
